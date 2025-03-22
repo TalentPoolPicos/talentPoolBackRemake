40c67d7a14276bf6fbe0a3043ac22276a7a59693
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2025-03-22 15:27:46 -0300

    feat: add Swagger documentation setup for development and enable CORS for production

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('API Documentation')
+      .setDescription('Interactive API documentation')
+      .setVersion('1')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    const theme = new SwaggerTheme();
+    const options = {
+      explorer: true,
+      customCss: theme.getBuffer(SwaggerThemeNameEnum.DRACULA),
+    };
+    SwaggerModule.setup('doc', app, document, options);
+  } else if (process.env.ENV === 'production') {
+    app.enableCors();
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
