6f47699d76054f0478ebcb05f3803b89829ff167
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2025-03-25 01:47:51 -0300

    feat: implement pagination and retrieval of users by UUID in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,92 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Query,
+} from '@nestjs/common';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UsersService } from './users.service';
+import { UsersPageDto } from './dtos/users_page.dto';
+import { UserDto } from 'src/dtos/user.dto';
 
 @ApiTags('User', 'V1')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @ApiOperation({ summary: 'Get users by pages' })
+  @ApiOkResponse({
+    description: 'The list of users and the total number of users',
+    type: UsersPageDto,
+  })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    type: Number,
+    description: 'The page number',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    description:
+      'The number of items per page. Default is 10. Max is 20 and min is 1',
+  })
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  async findByPage(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+  ) {
+    const result = await this.usersService.findAndCountAll(page, limit);
+    return {
+      users: result.users.map((user) => {
+        return {
+          uuid: user.uuid,
+          username: user.username,
+          email: user.email,
+          role: user.role,
+          profilePicture: user.profilePicture,
+          created_at: user.createdAt,
+          updated_at: user.updatedAt,
+        };
+      }),
+      total: result.total,
+    };
+  }
+
+  @ApiOperation({ summary: 'Get a user by uuid' })
+  @ApiOkResponse({
+    description: 'The user',
+    type: UserDto,
+  })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @HttpCode(HttpStatus.OK)
+  @Get(':uuid')
+  async findOne(@Param('uuid') uuid: string) {
+    const user = await this.usersService.findByUuid(uuid);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return {
+      uuid: user.uuid,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      profilePicture: user.profilePicture,
+      created_at: user.createdAt,
+      updated_at: user.updatedAt,
+    };
+  }
+}
