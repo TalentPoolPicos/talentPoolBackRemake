3829eddeabb5b00810050d88d88cb3ab84f40bb1
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2025-03-27 02:45:54 -0300

    feat: add Public decorator to Hello world endpoint for open access

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { Public } from './auth/decotaros/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @ApiOperation({ summary: 'Hello world' })
   @ApiOkResponse({ description: 'Hello world' })
   @Get()
