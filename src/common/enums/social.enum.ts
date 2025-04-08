2bc35ae91b228d82dcfac5e6ffd313455d06ef6f
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2025-04-08 01:09:00 -0300

    feat: add SocialMediaType enum for social media platforms

---
+++ src/common/enums/social.enum.ts
@@ -0,0 +1,14 @@
+export enum SocialMediaType {
+  X = 'X',
+  FACEBOOK = 'facebook',
+  INSTAGRAM = 'instagram',
+  LINKEDIN = 'linkedin',
+  YOUTUBE = 'youtube',
+  TIKTOK = 'tiktok',
+  GITHUB = 'github',
+  GITLAB = 'gitlab',
+  REDDIT = 'reddit',
+  TELEGRAM = 'telegram',
+  WHATSAPP = 'whatsapp',
+  DISCORD = 'discord',
+}
