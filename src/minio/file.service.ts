8bd9303acb1cc8579fd64a18d2de1bad33122e2c
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2025-04-18 07:50:07 -0300

    feat: implement MinIO service and module for file management

---
+++ src/minio/file.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import * as Minio from 'minio';
+import { InjectMinio } from 'src/minio/minio.decorator';
+
+@Injectable()
+export class FilesService {
+  protected _bucketName = 'main';
+
+  constructor(@InjectMinio() private readonly minioService: Minio.Client) {}
+
+  async bucketsList() {
+    return await this.minioService.listBuckets();
+  }
+
+  async getFileUrl(filename: string): Promise<string | null> {
+    try {
+      return this.minioService.presignedUrl('GET', this._bucketName, filename);
+    } catch {
+      return null;
+    }
+  }
+
+  async uploadFile(file: Express.Multer.File) {
+    const extension = file.originalname.split('.').pop();
+    const filename = `${randomUUID().toString()}.${extension}`;
+    const result = await this.minioService.putObject(
+      this._bucketName,
+      filename,
+      file.buffer,
+      file.size,
+      {
+        'Content-Type': file.mimetype,
+      },
+    );
+
+    return { result, filename };
+  }
+
+  async deleteFile(filename: string) {
+    return await this.minioService.removeObject(this._bucketName, filename);
+  }
+}
---
+++ src/minio/minio.module.ts
@@ -0,0 +1,28 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MINIO_TOKEN } from './minio.decorator';
+import * as Minio from 'minio';
+import { FilesService } from './file.service';
+
+@Global()
+@Module({
+  exports: [MINIO_TOKEN, FilesService],
+  providers: [
+    {
+      inject: [ConfigService],
+      provide: MINIO_TOKEN,
+      useFactory: (configService: ConfigService): Minio.Client => {
+        const client = new Minio.Client({
+          endPoint: configService.getOrThrow('MINIO_ENDPOINT'),
+          port: +configService.getOrThrow('MINIO_PORT'),
+          accessKey: configService.getOrThrow('MINIO_ACCESS_KEY'),
+          secretKey: configService.getOrThrow('MINIO_SECRET_KEY'),
+          useSSL: false,
+        });
+        return client;
+      },
+    },
+    FilesService,
+  ],
+})
+export class MinioModule {}
