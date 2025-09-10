797c7a32adc05b677990c987f6be05cb70fedcb3
Armando Luz <armandoari.288@gmail.com>
2025-09-10 18:45:58 -0300

    feat: add StorageModule to UsersModule imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { StorageModule } from '../storage/storage.module';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MeController } from './me.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, StorageModule],
   controllers: [UsersController, MeController],
   providers: [UsersService],
   exports: [UsersService],
