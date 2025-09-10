ffa9ed2b3766df24badff719f56ecfa22c7b01bb
Armando Luz <armandoari.288@gmail.com>
2025-09-10 17:24:24 -0300

    feat: refactor app module to remove TypeORM and integrate Prisma for database management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,44 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import { EnterpriseModule } from './enterprise/enterprise.module';
-import { StudentsModule } from './students/students.module';
-import { SocialmediaModule } from './socialmedia/socialmedia.module';
-import { MinioModule } from './minio/minio.module';
-import { TagsModule } from './tags/tags.module';
-import { SearchModule } from './search/search.module';
-import { AddressModule } from './address/address.module';
-import { LikeModule } from './likes/like.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST || 'localhost',
-      port: parseInt(process.env.POSTGRES_PORT || '5432'),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: process.env.DATABASE_SYNCHRONIZE === 'true', // Set to false in production
-    }),
-    MinioModule,
-    UsersModule,
-    AuthModule,
-    EnterpriseModule,
-    StudentsModule,
-    SocialmediaModule,
-    TagsModule,
-    SearchModule,
-    AddressModule,
-    LikeModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
   controllers: [AppController],
-
   providers: [AppService],
 })
 export class AppModule {}
